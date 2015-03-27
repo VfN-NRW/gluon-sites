74674ca48789a34a8839557b26e051b3756fbcd7
@RubenKelevra <ruben@freifunk-nrw.de>
2015-03-27 19:35:09 +0100

    remove unneeded modules

--- site-leverkusen/site.mk
+++ site-leverkusen/site.mk
@@ -1,14 +1,11 @@
 GLUON_SITE_PACKAGES := \
 	gluon-mesh-batman-adv-15 \
-	gluon-alfred \
-	gluon-announced \
 	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
-	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
