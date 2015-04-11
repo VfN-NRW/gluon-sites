292147d88188ac93eb30b006d4fb92891b842896
@RubenKelevra <ruben@freifunk-nrw.de>
2015-04-12 00:16:31 +0200

    update to v0.6.1prebeta2

--- all/site.mk
+++ all/site.mk
@@ -22,7 +22,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6.1beta1
+DEFAULT_GLUON_RELEASE := 0.6.1prebeta2+exp$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
