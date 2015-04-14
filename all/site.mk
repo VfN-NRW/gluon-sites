2213a4b5f5d5063ad364b656a51a39e799f5edbb
@RubenKelevra <ruben@freifunk-nrw.de>
2015-04-14 18:52:26 +0200

    update version number

--- all/site.mk
+++ all/site.mk
@@ -22,7 +22,7 @@ GLUON_SITE_PACKAGES := \
 	haveged
 
 
-DEFAULT_GLUON_RELEASE := 0.6.2+beta2-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.6.2-beta2
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
