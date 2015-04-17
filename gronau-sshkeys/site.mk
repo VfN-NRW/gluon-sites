da6ab9d7c3ad1f14bb5399678b168d41cdda64cd
@RubenKelevra <ruben@freifunk-nrw.de>
2015-04-17 10:32:41 +0200

    update gronau version number

--- gronau-sshkeys/site.mk
+++ gronau-sshkeys/site.mk
@@ -23,7 +23,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-authorized-keys
 
 
-DEFAULT_GLUON_RELEASE := 0.6.2-beta2
+DEFAULT_GLUON_RELEASE := 0.6.3-beta3
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
