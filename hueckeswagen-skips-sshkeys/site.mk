7dacedf54f4c1c2e7cb5a346d6b2a9b9ae279c28
Kevin Olbrich <ko@sv01.de>
2015-05-02 18:11:27 +0200

    ff-hw with ssh-keys

---
+++ hueckeswagen-skips-sshkeys/site.mk
@@ -0,0 +1,31 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-autoupdater \
+	gluon-offline-ssid \
+	gluon-legacy-https-stats \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-contact-info \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-private-wifi \
+	gluon-luci-portconfig \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-status-page \
+	iwinfo \
+	iptables \
+	haveged \
+	gluon-authorized-keys
+
+
+DEFAULT_GLUON_RELEASE := 0.6.5-prebeta6
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
