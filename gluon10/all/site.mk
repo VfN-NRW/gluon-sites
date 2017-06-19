cddedcfd686a67cec2b2da6364c5164225bb9a96
Felix Kaechele <felix@fetzig.org>
2017-06-19 18:42:17 +0200

    Enable USB gadgets for x86 (#5)
    * Enable USB gadgets for x86
    
    Also change site.mk so that it diffs nicely with upstream sample file
    by:
      - sorting package names alphabetically, where dep chain allows
      - removing redundant packages that are implicitly required
    
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>
    
    * Address review comments
    
    - remove multicast filters and status page
    - not set default release
    
    Signed-off-by: Felix Kaechele <felix@kaechele.ca>

--- gluon10/all/site.mk
+++ gluon10/all/site.mk
@@ -1,32 +1,84 @@
+##	GLUON_SITE_PACKAGES
+#		specify Gluon/LEDE packages to include here
+
 GLUON_SITE_PACKAGES := \
-	gluon-mesh-batman-adv-15 \
-	gluon-offline-ssid \
-	gluon-legacy-https-stats \
-	gluon-setup-mode \
-	gluon-config-mode-core \
+	gluon-autoupdater \
 	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-segment-mld \
 	gluon-ebtables-source-filter \
 	gluon-web-admin \
-	gluon-web-wifi-config \
 	gluon-web-autoupdater \
-	gluon-autoupdater \
-	gluon-web-private-wifi \
+	gluon-web-mesh-vpn-fastd \
 	gluon-web-network \
-	gluon-client-bridge \
+	gluon-web-private-wifi \
+	gluon-web-wifi-config \
+	gluon-mesh-batman-adv-15 \
 	gluon-mesh-vpn-fastd \
-	iwinfo \
-	iptables \
+	gluon-radvd \
+	gluon-setup-mode \
 	haveged \
-	gluon-ebtables-segment-mld
+	iptables \
+	iwinfo \
+	gluon-legacy-https-stats \
+	gluon-offline-ssid
 
-# Allow overriding the release number from the command line
+# Additional Modules for USB gadgets
+USB_BASIC := \
+	kmod-usb-core \
+	kmod-usb2 \
+	kmod-usb3 \
+	kmod-usb-hid
+
+USB_NIC := \
+	kmod-usb-net \
+	kmod-usb-net-asix \
+	kmod-usb-net-asix-ax88179 \
+	kmod-usb-net-cdc-ether \
+	kmod-usb-net-dm9601-ether \
+	kmod-usb-net-mcs7830 \
+	kmod-usb-net-pegasus \
+	kmod-usb-net-rtl8150 \
+	kmod-usb-net-rtl8152 \
+	kmod-usb-net-smsc95xx \
+	kmod-usb-net-sr9700
+
+USB_WIFI := \
+	kmod-rtl8192cu
+
+# Additional packages for x86-generic builds
+ifeq ($(GLUON_TARGET),x86-generic)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		kmod-usb-ohci-pci \
+		$(USB_NIC)
+endif
+
+# Additional packages for x86-64 builds
+ifeq ($(GLUON_TARGET),x86-64)
+	GLUON_SITE_PACKAGES += \
+		$(USB_BASIC) \
+		$(USB_NIC) \
+		kmod-igb # APU2
+endif
+
+# Variables set with ?= can be overwritten from the command line
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Default priority for updates.
 GLUON_PRIORITY ?= 0
 
 # Region code required for some images; supported values: us eu
@@ -34,4 +86,3 @@ GLUON_REGION ?= eu
 
 # Languages to include
 GLUON_LANGS ?= en de
-
