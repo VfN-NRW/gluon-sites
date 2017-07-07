74f1c265fb3247d007924a5f9b2d74a7e5c87e0f
@RubenKelevra <ruben@vfn-nrw.de>
2017-07-07 05:06:54 +0200

    remove segment mld filter

--- gluon10/all/site.mk
+++ gluon10/all/site.mk
@@ -11,7 +11,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
-	gluon-ebtables-segment-mld \
 	gluon-ebtables-source-filter \
 	gluon-ebtables-limit-arp \
 	gluon-web-admin \
